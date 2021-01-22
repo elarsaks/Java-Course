6e60905a189bd5dcfaf59806197325e5d172a3b6
elarsaks <elarsaks@gmail.com>
2021-01-22 20:22:53 +0200

    Create password.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,6 +7,7 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private int defaultPasswordLength = 10;
 	private int mailboxCapacity;
 	private String alternateEmail;
 	
@@ -19,6 +20,10 @@ public class Email {
 		// Call method asking for the department - return the department 
 		this.department = setDepartment();
 		System.out.println("Department is " + this.department);
+		
+		// Call a method that returns random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " + this.password);
 	}
 	
 	// Ask for department
@@ -36,6 +41,17 @@ public class Email {
 	}
 	
 	// Generate random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPRSTUWXYZ0123456789!@#$";
+		char[] password = new char[length];
+		
+		for (int i=0; i<length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		
+		return new String(password);
+	}
 	
 	// Set the mailbox capacity
 	
