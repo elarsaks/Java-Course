62c163c7caa3e89208b668c89b3a38e396ea8cf2
elarsaks <elarsaks@gmail.com>
2021-01-24 19:04:39 +0200

    Add transactions.

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -5,10 +5,14 @@ public class BankAccountApp {
 	public static void main(String[] args) {
 		
 		Checking chkacc1 = new Checking("Tom Wilson", "548615482", 1500);
-		Savings savacc = new Savings("Rich Lowe", "562485285", 2500);
+		Savings savacc1 = new Savings("Rich Lowe", "562485285", 2500);
 		
 		chkacc1.showInfo();
-		savacc.showInfo();
+		savacc1.showInfo();
+		
+		savacc1.deposit(5000);
+		savacc1.withdraw(200);
+		savacc1.transfer("Brokerage", 3000);
 		
 		// Read CSV file then create new accounts based on that data
 
