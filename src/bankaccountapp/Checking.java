ec49e8fc659efb03ec02efdb791f7c10bd4aab6c
elarsaks <elarsaks@gmail.com>
2021-01-24 18:17:00 +0200

    Add savings and checking accounts safety details.

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -2,19 +2,29 @@ package bankaccountapp;
 
 public class Checking extends Account {
 	// List properties for checking account
-	int debitCaredNumber;
+	int debitCardNumber;
 	int debitCardPIN;
 	
 	// Constructor to initialize checking account
 	public Checking(String name, String sSN, double initDeposit) {
 		super(name, sSN, initDeposit);
 		accountNumber = "2" + accountNumber;
+		setDebitCard();
+	}
+	
+	private void setDebitCard() {
+		debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+		debitCardPIN = (int) (Math.random() * Math.pow(10, 4));
 	}
 	
 	// List any methods specific to the checking account
 	public void showInfo() {
 		super.showInfo();
-		System.out.println("ACCOUNT TYPE: Cheking \n");
+		System.out.println(
+				" Your Checking Account Features: " + 
+				"\n Debit Card Number: " + debitCardNumber +
+				"\n Debit Card PIN: " + debitCardPIN
+				);
 	}
 
 }
